e69ff8ab6e760fe3bb51d30ef3a85d62719e1116
YOGA <art96962006@gmail.com>
2016-10-31 16:46:26 +0800

    A practice for Js in 2016.10.31.

---
+++ Practice4/load_restaurants.js
@@ -0,0 +1,29 @@
+var table = document.createElement("table");
+table.style.border = "1px solid #000000";
+
+var table_body = document.createElement("tbody");
+var table_head = document.createElement("thead");
+var name_head = document.createElement("th");
+var time_head = document.createElement("th");
+
+name_head.appendChild(document.createTextNode("商店"));
+name_head.style.border = "1px solid #000000";
+time_head.appendChild(document.createTextNode("營業時間"));
+time_head.style.border = "1px solid #000000";
+table_head.appendChild(name_head);
+table_head.appendChild(time_head);
+
+for (var i = 0; i < restaurants.length; i++) {
+    var row = document.createElement("tr");
+    for (key in restaurants[i]) {
+        var cell = document.createElement("td");
+        cell.appendChild(document.createTextNode(restaurants[i][key]));
+        cell.style.border = "1px solid #000000";
+        row.appendChild(cell);
+    }
+    table_body.appendChild(row);
+}
+
+table.appendChild(table_head);
+table.appendChild(table_body);
+document.body.appendChild(table);
